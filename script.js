8dad2be5d283aedfe0f9957a98ac77a94abe7036
Ankit <136968673+AnkitKumarDelta@users.noreply.github.com>
2025-11-12 20:50:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,18 @@
 function threeSum(arr, target) {
-  //your code here
+  let maxi = Infinity;
+	let result = [];
+	for(let i=0;i<=arr.length-3;i++){
+		for(let j=i+1;j<=arr.length-2;j++){
+			let sum = arr[i]+arr[j];
+			for(let k=j+1;k<=arr.length-1;k++){
+				sum+=arr[k];
+				if(Math.abs(sum-target)<maxi){
+					result = [arr[i],arr[j],arr[k]];
+				}
+			}
+		}
+	}
+	return result;
 }
 
 module.exports = threeSum;
